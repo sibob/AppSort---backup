532c009ae2786c1cf6cc2e243a450c89155455b1
Simon Burrill <simonburrill@gmail.com>
2025-08-10 17:04:17 +0100

    Initial Commit

---
+++ AppSortTests/AppSortTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppSortTests.swift
+//  AppSortTests
+//
+//  Created by Simon Burrill on 10/08/2025.
+//
+
+import Testing
+@testable import AppSort
+
+struct AppSortTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
